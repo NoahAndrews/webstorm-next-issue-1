6b0099137ddcb13f9f662692c90c958eb56bccc6
NoahAndrews <noah@revrobotics.com>
2024-03-18 11:45:55 -0500

    Add disclaimer

---
+++ src/ReactRenderMethodThatReturnsNull.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+/**
+ * Unfortunately, this file only shows phantom errors when in the context of my larger project
+ */
+export class ReactRenderMethodThatReturnsNull extends React.Component<any, any> {
+    render() {
+        return null;
+    }
+}
